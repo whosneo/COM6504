8909c4249127475f2d4f36e2dd0fbcee079f6f1f
Bowen Yu <byu8@sheffield.ac.uk>
2019-03-27 19:13:24 +0000

    feat: capture screenshots at fixed size

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -1,75 +1,65 @@
-/*
-Copyright 2017 Google Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 'use strict';
 
-var videoElement = document.querySelector('video');
-var audioSelect = document.querySelector('select#audioSource');
-var videoSelect = document.querySelector('select#videoSource');
+let videoElement = document.querySelector('video');
+// const audioSelect = document.querySelector('select#audioSource');
+const videoSelect = document.querySelector('select#videoSource');
+const canvas = document.querySelector('canvas');
 
-navigator.mediaDevices.enumerateDevices()
-    .then(gotDevices).then(getStream).catch(handleError);
+navigator.mediaDevices.enumerateDevices().then(gotDevices).then(getStream).catch(handleError);
 
-audioSelect.onchange = getStream;
+// audioSelect.onchange = getStream;
 videoSelect.onchange = getStream;
 
 function gotDevices(deviceInfos) {
-  for (var i = 0; i !== deviceInfos.length; ++i) {
-    var deviceInfo = deviceInfos[i];
-    var option = document.createElement('option');
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === 'audioinput') {
-      option.text = deviceInfo.label ||
-          'microphone ' + (audioSelect.length + 1);
-      audioSelect.appendChild(option);
-    } else if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || 'camera ' +
-          (videoSelect.length + 1);
-      videoSelect.appendChild(option);
-    } else {
-      console.log('Found one other kind of source/device: ', deviceInfo);
+    for (let i = 0; i < deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'audioinput') {
+            // option.text = deviceInfo.label || 'microphone ' + (audioSelect.length + 1);
+            // audioSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || 'camera ' + (videoSelect.length + 1);
+            videoSelect.appendChild(option);
+        } else {
+            console.log('Found one other kind of source/device: ', deviceInfo);
+        }
     }
-  }
 }
 
 function getStream() {
-  if (window.stream) {
-    window.stream.getTracks().forEach(function(track) {
-      track.stop();
-    });
-  }
-
-  var constraints = {
-    audio: {
-      deviceId: {exact: audioSelect.value}
-    },
-    video: {
-      deviceId: {exact: videoSelect.value}
+    if (window.stream) {
+        window.stream.getTracks().forEach(function (track) {
+            track.stop();
+        });
     }
-  };
 
-  navigator.mediaDevices.getUserMedia(constraints).
-  then(gotStream).catch(handleError);
+    const constraints = {
+        audio: true,
+        video: {
+            deviceId: videoSelect.value ? {exact: videoSelect.value} : true,
+            frameRate: {min: 10}
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
 }
 
 function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  videoElement.srcObject = stream;
+    window.stream = stream; // make stream available to console
+    videoElement.srcObject = stream;
 }
 
 function handleError(error) {
-  console.log('Error: ', error);
+    console.log('Error: ', error);
+}
+
+function snapshot() {
+    if (window.stream) {
+        let context = canvas.getContext('2d');
+        // context.fillRect(0,0, 640, 480);
+        context.drawImage(videoElement, 0, 0, 640, 480);
+        document.querySelector('img').src = canvas.toDataURL('image/png');
+
+    }
 }
